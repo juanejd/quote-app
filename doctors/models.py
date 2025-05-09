8e71dfd13dfca3c98193c41905e2aaa4a3a34f63
juanejd <juanes.pio51@gmail.com>
2025-05-09 09:22:51 -0500

    Creating Models in the apps

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Tabla doctor
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField()
+    biography = models.TextField()
+
+# Tabla departamento
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+# Tabla disponibilidad del doctor
+class DoctorAvailability(models.Model):
+    doctor = models.ForeignKey(Doctor, related_name= 'medical_notes', on_delete=models.CASCADE)
+    note = models.TextField()
+    date = models.DateField()
