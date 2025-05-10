af791d7d30a7b6ed3f0944a9de513d80ccfd0d09
juanejd <juanes.pio51@gmail.com>
2025-05-10 15:08:45 -0500

    Refactoring classes views in patients,  using generic views

--- patients/views.py
+++ patients/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 
 # GET /api/patients => Listar
 # POST /api/patients => Crear
@@ -12,55 +13,20 @@ from rest_framework.views import APIView
 # PUT /api/patients/<pk> => Modificacion
 # DELETE /api/patients/<pk> => Borrar
 
-# Vista basada en clase
-class ListPatientsView(APIView):
-    allowed_methods = ['GET', 'POST']
+# Vista basada en clases genericas, ListAPIView para el get y CreateAPIView para el post
+class ListPatientsView(ListAPIView, CreateAPIView):
+    # parametros a usar en los metodos GET y POST
+    allowed_methods = ['GET', 'POST'] # Metodos permitidos
+    serializer_class = PatientSerializer #Serializer a utilizar
+    queryset = Patient.objects.all() #Query que se envia a la base de datos
 
 
-    def get(self, request):
-        patients = Patient.objects.all() # Obtenemos todos los pacientes
-        serializer = PatientSerializer(patients, many = True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = PatientSerializer(data=request.data) # Validar informacion digitada por el usuario
-        serializer.is_valid(raise_exception=True) # con raise exception mostrarmos el error generado en formato json en caso de que la data haya sido ingresada incorrectamente
-        serializer.save() # Guardamos
-        return Response(status=status.HTTP_201_CREATED)
-
-# vista basada en clase
-class DetailPatientView(APIView):
+# vista basada en clase genericoas, RetrieveAPIView get cada pk, UpdateAPIView para el put y   DestroyAPIView para el delete
+class DetailPatientView(RetrieveAPIView, UpdateAPIView, DestroyAPIView):
     allowed_methods = ['GET', 'PUT', 'DELETE']
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
 
-    def get(self, request, pk):
-        # Creamos un try-except en caso de que no exista el usuario solicitado
-        try:
-            patient = Patient.objects.get(id=pk) # Query para acceder a la informacion de 1 paciente con su id
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)# En caso de que no exista Lanzamos un error 404 not_found 
-        serializer = PatientSerializer(patient) # Si existe el usuario, usamos la data de ese paciente dentro del serializer
-        return Response(serializer.data) # retornar la data en formato json
-    
-    def put(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk) 
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = PatientSerializer(patient, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk) 
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        patient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Decorador para que django rest sea compatible con la vista
 # @api_view(['GET', 'POST']) #solo es compatible con el get para no usar esta misma vista en otra url
