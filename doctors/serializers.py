e8b0d29a6261df8ec9d5b19531a66acc153a646a
juanejd <juanes.pio51@gmail.com>
2025-05-11 19:13:18 -0500

    creating custom validations in the fields models through his serializer

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -8,6 +8,22 @@ class DoctorSerializer(serializers.ModelSerializer):
         model = Doctor
         fields = "__all__"
 
+    # Validación personalizada en los doctores, donde el correo debe contener el dominio @example.com
+    def validate_email(self, value):
+        if "@example.com" in value:
+            return value
+        # usamos un raise de serializer "ValidationError"
+        raise serializers.ValidationError("El correo debe incluir @example.com")
+
+    # Validación personalizada con múltiples campos, en este caso el 'contact_number' y el 'is_on_vacation'
+    def validate(self, attrs):
+        # attrs = donde almacenamos los campos de la tabla 'doctors'
+        if len(attrs["contact_number"]) < 10 and attrs["is_on_vacation"]:
+            raise serializers.ValidationError(
+                "Por favor, ingrese un numero antes de irte a vacaciones"
+            )
+        return super().validate(attrs)
+
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
--- patients/serializers.py
+++ patients/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from datetime import date
 
 from .models import Patient, Insurance, MedicalRecord
 
@@ -7,6 +8,27 @@ class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = "__all__"
+
+    def validate_date_of_birth(self, value):
+        """
+        Valida que la fecha de nacimiento no sea futura
+        """
+        today = date.today()
+        if value > today:
+            raise serializers.ValidationError(
+                "La fecha de nacimiento no puede ser una fecha futura"
+            )
+        return value
+
+    def validate_address(self, value):
+        """
+        Validamos que la direccion sea mayor a 5 caracteres
+        """
+        if len(value.strip()) < 5:
+            raise serializers.ValidationError(
+                "La dirección debe tener al menos 5 caracteres"
+            )
+        return value
 
 
 class InsuranceSerializer(serializers.ModelSerializer):
